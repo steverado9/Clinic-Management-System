e9d3d8348d78c58b0077c6368f066bdb12ecff75
stephen <isaacsteverado9@gmail.com>
2026-03-23 02:04:45 +0100

    first commit

---
+++ src/main/java/com/steverado9/Clinic/management/system/ClinicManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.steverado9.Clinic.management.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicManagementSystemApplication.class, args);
+	}
+
+}
