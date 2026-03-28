abcc24fcf0af3ee4b9cc1eed2f0ee43ccfc2095b
stephen <isaacsteverado9@gmail.com>
2026-03-28 01:47:31 +0100

    new commit

--- src/main/java/com/steverado9/Clinic/management/system/SecurityConfig/SecurityConfig.java
+++ src/main/java/com/steverado9/Clinic/management/system/SecurityConfig/SecurityConfig.java
@@ -1,45 +1,45 @@
-//package com.steverado9.Clinic.management.system.SecurityConfig;
-//
-////import com.steverado9.Clinic.management.system.service.CustomUserDetailsService;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-////    private final CustomUserDetailsService userDetailsService;
-//
-////    public SecurityConfig(CustomUserDetailsService userDetailsService) {
-////        this.userDetailsService = userDetailsService;
-////    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-////    @Bean
-////    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-////
-////        http.authorizeHttpRequests(auth -> auth
-////                .requestMatchers("/login", "/register/**").permitAll()
-////                .requestMatchers("/admin/**").hasRole("ADMIN")
-////                .requestMatchers("/doctor/**").hasRole("DOCTOR")
-////                .requestMatchers("/reception/**").hasRole("RECEPTIONIST")
-////                .requestMatchers("/patient/**").hasRole("PATIENT")
-////                .anyRequest().authenticated()
-////        ).formLogin(login -> login
-////                .loginPage("/login")
-////                .usernameParameter("email")
-////                .defaultSuccessUrl("/dashboard", true)
-////                .permitAll()
-////        ).logout(logout -> logout.logoutSuccessUrl("/login?logout"));
-////        return http.build();
-////    }
-//}
+package com.steverado9.Clinic.management.system.SecurityConfig;
+
+import com.steverado9.Clinic.management.system.service.CustomUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final CustomUserDetailsService userDetailsService;
+
+    public SecurityConfig(CustomUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/login", "/register/**").permitAll()
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/doctor/**").hasRole("DOCTOR")
+                .requestMatchers("/reception/**").hasRole("RECEPTIONIST")
+                .requestMatchers("/patient/**").hasRole("PATIENT")
+                .anyRequest().authenticated()
+        ).formLogin(login -> login
+                .loginPage("/login")
+                .usernameParameter("email")
+                .defaultSuccessUrl("/dashboard", true)
+                .permitAll()
+        ).logout(logout -> logout.logoutSuccessUrl("/login?logout"));
+        return http.build();
+    }
+}
